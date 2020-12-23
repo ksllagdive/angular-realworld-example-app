9e94f7417ae2e2bfa685261e851daf6626152fc7
Kashinath Lagdive <kashinathl@cybage.com>
2020-12-23 17:45:03 +0530

    my first changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,9 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
+// api_url: 'https://conduit.productionready.io/api'
 
 export const environment = {
   production: false,
-  api_url: 'https://conduit.productionready.io/api'
+  api_url: 'http://localhost:8080/api'
 };
